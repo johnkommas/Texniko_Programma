d040f90c3df25543b1f3011051417ba6f7f2f244
Ioannis E. Kommas <54149422+johnkommas@users.noreply.github.com>
2023-09-07 23:09:23 +0300

    Add setup.py file
    This commit introduces a new setup.py file for the 'Texniko_Programma' package. The setup.py file is crucial for defining package metadata and facilitates the package's distribution and installation. The metadata includes the package's name, version, author information, and a brief description of the package. Adding a setup.py file is a standard, good practice in Python projects, contributing to better maintainability and ease of installation for users.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Texniko_Programma',
+    version='1.0.0',
+    packages=[''],
+    url='https://github.com/johnkommas/Texniko_Programma',
+    license='MIT',
+    author='Ioannis E. Kommas',
+    author_email='johnkommas@gmail.com',
+    description='A package for creating and formatting excel workbooks for Texniko Programma data handling.'
+)
